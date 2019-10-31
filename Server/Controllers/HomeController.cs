7f10d743d8938893318f108d294141ccbe9bc9ab
Roman Mashenkin <xromash@vxdesign.store>
2019-10-31 23:44:53 +0300

    [#1] Setup Server application
    Signed-off-by: Roman Mashenkin <xromash@vxdesign.store>

---
+++ Server/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return File("~/index.html", "text/html");
+        }
+
+        [Route("Error")]
+        public IActionResult Error()
+        {
+            return BadRequest("Error page");
+        }
+    }
+}
---
+++ Server/Startup.cs
@@ -0,0 +1,71 @@
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+
+namespace Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.Map(new PathString("/api"), builder =>
+            {
+                builder.Run(async context =>
+                {
+                    context.Response.StatusCode = 400;
+                    context.Response.ContentType = "application/json";
+                    var msg = JsonConvert.SerializeObject(new { Message = "Invalid request path" });
+                    await context.Response.WriteAsync(msg).ConfigureAwait(false);
+                });
+            });
+
+            app.Run(async context =>
+            {
+                context.Response.ContentType = "text/html";
+                await context.Response.SendFileAsync(Path.Combine(env.WebRootPath, "index.html"));
+            });
+        }
+    }
+}
