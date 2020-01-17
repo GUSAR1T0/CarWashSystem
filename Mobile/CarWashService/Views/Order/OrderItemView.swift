b85bf4718cab2534308f88aa87674bbc288389ec
Anna Boykova <boykova_anuta@mail.ru>
2020-01-17 08:32:36 +0300

    [#26] Appointments view for mobile

---
+++ Mobile/CarWashService/Views/Order/OrderItemView.swift
@@ -0,0 +1,21 @@
+//
+//  OrderItemView.swift
+//  CarWashService
+//
+//  Created by Anna Boykova on 17.01.2020.
+//  Copyright © 2020 VXDESIGN.STORE. All rights reserved.
+//
+
+import SwiftUI
+
+struct OrderItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderItemView()
+    }
+}
